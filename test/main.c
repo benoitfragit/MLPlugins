c3a8b0f2f1b038547c578fb03f2871166e166527
Benoit Franquet <benoitfranquet@gmail.com>
2016-04-23 21:35:20 +0200

    adding data parsing utility

--- test/main.c
+++ test/main.c
@@ -2,22 +2,43 @@
 #include <stdio.h>
 
 #include "brain.h"
+#include "brain_data_reader.h"
 
 int
 main(int argc, char** argv)
 {
-    Document doc = open_document("/home/benoit/Documents/projets/C/libBrain/test/mapping.xml");
+    Network* network = NULL;
+    Data*    data    = NULL;
 
-    if (doc)
+    if (argc <= 2)
     {
-        Network* network = new_network_from_context(get_root_node(doc));
+        fprintf(stderr, "<%s, %d>: you should pass the network xml file and the data xml file\n", __FILE__, __LINE__);
+        return EXIT_FAILURE;
+    }
 
-        if (network)
-        {
-            delete_network(network);
-        }
+    Document network_document = open_document(argv[1]);
+    Document data_document    = open_document(argv[2]);
 
-        close_document(doc);
+    if (network_document)
+    {
+        network = new_network_from_context(get_root_node(network_document));
+        close_document(network_document);
+    }
+
+    if (data_document)
+    {
+        data = new_data_from_context(get_root_node(data_document));
+        close_document(data_document);
+    }
+
+    if (network)
+    {
+        delete_network(network);
+    }
+
+    if (data)
+    {
+        delete_data(data);
     }
 
     return EXIT_SUCCESS;
