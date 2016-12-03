d9039e3e004d23e8a6633d12bbf077628b03978e
Benoit Franquet <benoitfranquet@gmail.com>
2016-12-03 18:34:51 +0100

    Removing bad doc

--- src/core/brain.h
+++ src/core/brain.h
@@ -4,46 +4,8 @@
 #include "brain_network_builder.h"
 #include "brain_data_reader.h"
 
-/* -------------------------------*/
-/**
- * @Synopsis
- *
- * Propate a Signal from the input
- * layer to the output layer
- *
- * @Param network
- */
-/* ---------------------------------*/
-void feedforward(Network_t network);
-
-/* -------------------------------*/
-/**
- * @Synopsis  
- *
- * Backpropagate the error from the
- * output layer to the input layer
- *
- * @Param network the NeuralNetwork
- * @Param number_of_output the size of the output vector
- * @Param desired The desired output
- *
- * @Returns The network quadratic error
- */
-/* ---------------------------------*/
+void   feedforward  (Network_t network);
 double backpropagate(Network_t network, const int number_of_output, const double *desired);
-
-/* -------------------------------*/
-/**
- * @Synopsis  
- *
- * Train a NeuralNetwork with backpropagation learning
- *
- * @Param network The network to train
- * @Param data    The data input signal and desired output
- * @Param target_error the target error 
- * @Param max_iter maximum number of iteration
- */
-/* ---------------------------------*/
-void train(Network_t network, const Data_t data, const double target_error, const int max_iter);
+void   train        (Network_t network, const Data_t data, const double target_error, const int max_iter);
 
 #endif /* BRAIN_H */
