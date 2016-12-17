dbc5f8ac4ec93a0b2197a6545ac7be5ae1049b45
Benoit Franquet <benoitfranquet@gmail.com>
2016-12-17 16:19:47 +0100

    adding BrainData doc

---
+++ docs/brain__data__reader_8h.js
@@ -0,0 +1,10 @@
+var brain__data__reader_8h =
+[
+    [ "delete_data", "brain__data__reader_8h.html#a5c28287f9682674b626e6e01dacae305", null ],
+    [ "get_data_input", "brain__data__reader_8h.html#ac5c16f1d904807423136f09a35ac3305", null ],
+    [ "get_data_input_length", "brain__data__reader_8h.html#a042d67d146c4a88afd7ba4febfd7cb13", null ],
+    [ "get_data_output", "brain__data__reader_8h.html#af86bd7390f646a72c63ec78f34643c7d", null ],
+    [ "get_data_output_length", "brain__data__reader_8h.html#a9c1590c4134d47b33f8b9665fa3559ab", null ],
+    [ "get_data_random_subset_index", "brain__data__reader_8h.html#a054cc968ff8383d3b163812441d3dcf3", null ],
+    [ "new_data_from_context", "brain__data__reader_8h.html#ace677f73508ab8c366c0fe4f82ce2f90", null ]
+];
