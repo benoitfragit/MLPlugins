adea58ffb1ae536767306e95b3d1a80f9e40236f
Benoit Franquet <benoitfranquet@gmail.com>
2019-01-20 16:48:53 +0100

    Adding trainer

---
+++ brain_trainer_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+PLUGIN_NAME=MLP
+XML_FOLDER=./plugins/MLP/example
+TRAINER=./install/debug/libbrain-1.0.0/bin/BrainTrainer
+
+export LD_LIBRARY_PATH=./install/debug/libbrain-1.0.0/lib/
+$TRAINER -p MLP \
+         -n $XML_FOLDER/test_train_network.xml \
+         -s $XML_FOLDER/test_train_settings.xml \
+         -d $XML_FOLDER/test_train_data.xml \
+         -r /tmp/serialized_network.xml
