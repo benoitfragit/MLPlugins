7f87d24050719323266dfc544504d19011ce1641
Benoit Franquet <benoitfranquet@gmail.com>
2019-07-25 21:21:19 +0200

    Removing test utils

--- models/MLP/test/src/common_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMON_TEST_H
-#define COMMON_TEST_H
-
-#include "brain_core_types.h"
-
-BrainInt compare_networks(const BrainNetwork left, const BrainNetwork right);
-
-#endif /* COMMON_TEST_H */
