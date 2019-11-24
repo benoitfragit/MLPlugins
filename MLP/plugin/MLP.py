d8228ce09c3da282724b633a58d619b842b341e0
Benoit Franquet <benoitfranquet@gmail.com>
2019-11-24 16:25:31 +0100

    Adding MLPModelManager

--- MLP/plugin/MLP.py
+++ MLP/plugin/MLP.py
@@ -14,6 +14,23 @@ from ui import MLPTrainerLoaderUI
 from ui import MLPTrainerEditorUI
 from ui import MLPNetworkDrawerUI
 
+class MLPModelManager:
+    def __init__(self, internal, key = None):
+        self._internal  = internal
+        self._key       = None
+        self._exception = None
+
+    def __enter__(self):
+        ret = None
+        try:
+            ret = self._internal[self._key]
+        except Exception as e:
+            self._exception = e
+        return ret
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        return True
+
 class MLPlugin(MLPluginBase, MLPLoader):
     def __init__(self):
         MLPluginBase.__init__(self)
@@ -26,18 +43,36 @@ class MLPlugin(MLPluginBase, MLPLoader):
 
     def load(self):
         # Call plugin init method
-        if self.init is not None:
-           self.init()
+        if self.mlp_plugin_init is not None:
+           self.mlp_plugin_init()
            self._activated = True
 
         # Get all metadata
-        if  self.metadata is not None:
-            meta = self.metadata().contents
+        if  self.mlp_plugin_metadata is not None:
+            meta = self.mlp_plugin_metadata().contents
             self._name      = meta.name
             self._version   = meta.version
             self._author    = meta.author
             self._description = meta.description
 
+    def mlptrainerwrapper(f):
+        wrapfunc = None
+
+        if f is not None:
+            print f
+            def wrapfunc(trainer, *args):
+                ret = None
+                with MLPModelManager(trainer, 'model') as model:
+                    if len(args) > 0:
+                        ret = f(trainer['model'], *args)
+                    else:
+                        ret = f(trainer['model'])
+                return ret
+        else:
+            def wrapfunc(trainer, *args):
+                pass
+
+        return wrapfunc
     """
     ....................................................................
     .......................... Plugin TRINER api........................
@@ -45,52 +80,60 @@ class MLPlugin(MLPluginBase, MLPLoader):
     """
     def mlGetTrainer(self, net, data):
         model = None
-        if self.newTrainer is not None:
-            model = self.newTrainer(net, data)
+        if self.mlp_trainer_new is not None:
+            model = self.mlp_trainer_new(net, data)
         return model
 
     def mlDeleteTrainer(self, trainer):
-        if trainer is not None and 'model' in trainer.keys() and self.deleteTrainer is not None:
-            self.deleteTrainer(trainer['model'])
+        with MLPModelManager(trainer, 'model') as model:
+            if self.mlp_trainer_delete is not None:
+                self.mlp_trainer_delete(trainer['model'])
 
     def mlConfigureTrainer(self, trainer, path):
-        if trainer is not None :
-            if 'model' in trainer.keys() and self.configureTrainer is not None:
-                self.configureTrainer(trainer['model'], path)
-            if 'settings' in trainer.keys():
-                trainer['settings'] = path
+        with MLPModelManager(trainer, 'model') as model:
+            if self.mlp_trainer_configure is not None:
+                self.mlp_trainer_configure(trainer['model'], path)
+        with MLPModelManager(trainer, 'settings') as settings:
+            trainer['settings'] = path
 
     def mlIsTrainerRunning(self, trainer):
         ret = False
-        if trainer is not None and 'model' in trainer.keys() and self.isTrainerRunning is not None:
-            ret = self.isTrainerRunning(trainer['model'])
+        with MLPModelManager(trainer, 'model') as model:
+            if self.mlp_trainer_is_running is not None:
+                ret = self.mlp_trainer_is_running(trainer['model'])
         return ret
 
     def mlGetTrainerProgress(self, trainer):
         ret = 0.0
-        if trainer is not None and 'model' in trainer.keys() and self.getTrainerProgress is not None:
-            ret = self.getTrainerProgress(trainer['model'])
+        with MLPModelManager(trainer, 'model') as model:
+            if self.mlp_trainer_get_progress is not None:
+                ret = self.mlp_trainer_get_progress(trainer['model'])
         return ret
 
+    #@mlptrainerwrapper(lambda x : x.mlp_trainer_run)
     def mlTrainerRun(self, trainer):
-        if trainer is not None and 'model' in trainer.keys() and self.runTrainer is not None:
-            self.runTrainer(trainer['model'])
+        with MLPModelManager(trainer, 'model') as model:
+            if self.mlp_trainer_run is not None:
+                self.mlp_trainer_run(trainer['model'])
 
     def mlGetTrainerError(self, trainer):
         ret = 100.0
-        if trainer is not None and 'model' in trainer.keys() and self.getTrainerError is not None:
-            ret = self.getTrainerError(trainer['model'])
+        with MLPModelManager(trainer, 'model') as model:
+            if self.mlp_trainer_error is not None:
+                ret = self.mlp_trainer_error(trainer['model'])
         return ret
 
     def mlSaveTrainerProgression(self, trainer, path):
-        if trainer is not None and 'model' in trainer.keys() and self.saveTrainerProgress is not None:
-            real_path = path + '.xml'
-            self.saveTrainerProgress(trainer['model'], real_path)
+        with MLPModelManager(trainer, 'model') as model:
+            if self.mlp_trainer_save_progression is not None:
+                real_path = path + '.xml'
+                self.mlp_trainer_save_progression(trainer['model'], real_path)
 
     def mlRestoreTrainerProgression(self, trainer, path, progress, error):
-        if trainer is not None and 'model' in trainer.keys() and self.restoreTrainerProgress is not None:
-            real_path =  path + '.xml'
-            self.restoreTrainerProgress(trainer['model'], real_path, progress, error)
+        with MLPModelManager(trainer, 'model') as model:
+            if self.mlp_trainer_restore_progression is not None:
+                real_path =  path + '.xml'
+                self.mlp_trainer_restore_progression(trainer['model'], real_path, progress, error)
 
     def mlGetLoadedTrainer(self):
         ret = None
@@ -106,11 +149,11 @@ class MLPlugin(MLPluginBase, MLPLoader):
         return ret
 
     def mlTrainerJSONEncoding(self, trainer, d):
-        if 'network' in trainer.keys():
+        with MLPModelManager(trainer, 'network') as network:
             d['network']  = trainer['network']
-        if 'data' in trainer.keys():
+        with MLPModelManager(trainer, 'data') as data:
             d['data']     = trainer['data']
-        if 'settings' in trainer.keys():
+        with MLPModelManager(trainer, 'settings') as settings:
             d['settings'] = trainer['settings']
 
     def mlTrainerJSONDecoding(self, buf):
@@ -159,8 +202,9 @@ class MLPlugin(MLPluginBase, MLPLoader):
     def mlTrainerGetManagedNetwork(self, trainer):
         internal = {}
 
-        if 'model' in trainer.keys() and self.getTrainerManagedNetwork is not None:
-            internal['model'] = self.getTrainerManagedNetwork(trainer['model'])
+        with MLPModelManager(trainer, 'model') as model:
+            if self.mlp_trainer_get_network is not None:
+                internal['model'] = self.mlp_trainer_get_network(trainer['model'])
 
         return internal
 
@@ -171,60 +215,69 @@ class MLPlugin(MLPluginBase, MLPLoader):
     """
     def mlGetNetwork(self, path):
         internal = {}
-        if self.newNetwork is not None:
-            internal['model'] = self.newNetwork(path)
+        if self.mlp_network_new is not None:
+            internal['model'] = self.mlp_network_new(path)
         return internal
 
     def mlDeleteNetwork(self, network):
-        if 'model' in network.keys() and self.deleteNetwork is not None:
-            self.deleteNetwork(network['model'])
+        with MLPModelManager(network, 'model') as model:
+            if self.mlp_network_delete is not None:
+                self.mlp_network_delete(network['model'])
 
     def mlSaveNetwork(self, network, path):
-        if 'model' in network.keys() and self.serializeNetwork is not None:
-            self.serializeNetwork(network['model'], path)
+        with MLPModelManager(network, 'model') as model:
+            if self.mlp_network_serialize is not None:
+                self.mlp_network_serialize(network['model'], path)
 
     def mlLoadNetwork(self, network, path):
-        if 'model' in network.keys() and self.deserializeNetwork is not None:
-            self.deserializeNetwork(network['model'], path)
+        with MLPModelManager(network, 'model') as model:
+            if self.mlp_network_deserialize is not None:
+                self.mlp_network_deserialize(network['model'], path)
 
     def mlPredict(self, network, num, sig):
-        if 'model' in network.keys() and self.predict is not None:
-            self.predict(network['model'], num, sig)
+        with MLPModelManager(network, 'model') as model:
+            if self.mlp_network_predict is not None:
+                self.mlp_network_predict(network['model'], num, sig)
 
     def mlGetNetworkOutputLength(self, network):
         ret = 0
-        if 'model' in network.keys() and self.getNetworkOutputLength is not None:
-            ret = self.getNetworkOutputLength(network['model'])
+        with MLPModelManager(network, 'model') as model:
+            if self.mlp_network_get_output_length is not None:
+                ret = self.mlp_network_get_output_length(network['model'])
         return ret
 
     def mlGetNetworkPrediction(self, network):
         ret = None
-        if 'model' in network.keys() and self.getNetworkOutput is not None:
-            ret = self.getNetworkOutput(network['model'])
+        with MLPModelManager(network, 'model') as model:
+            if self.mlp_network_get_output is not None:
+                ret = self.mlp_network_get_output(network['model'])
         return ret
 
     def mlGetNetworkNumberOfLayer(self, network):
         ret = 0
-        if 'model' in network.keys() and self.getNetworkNumberOfLayer is not None:
-            ret = self.getNetworkNumberOfLayer(network['model'])
+        with MLPModelManager(network, 'model') as model:
+            if self.mlp_network_get_number_of_layer is not None:
+                ret = self.mlp_network_get_number_of_layer(network['model'])
         return ret
 
     def mlGetNetworkNumberOfInput(self, network):
         ret = 0
-        if 'model' in network.keys() and self.getNetworkNumberOfInput is not None:
-            ret = self.getNetworkNumberOfInput(network['model'])
+        with MLPModelManager(network, 'model') as model:
+            if self.mlp_network_get_number_of_input is not None:
+                ret = self.mlp_network_get_number_of_input(network['model'])
         return ret
 
     def mlGetLayerNumberOfNeuron(self, network, i):
         ret = 0
-        if 'model' in network.keys() and self.getNetworkLayerNumberOfNeuron is not None:
-            ret = self.getNetworkLayerNumberOfNeuron(network['model'], i)
+        with MLPModelManager(network, 'model') as model:
+            if self.mlp_network_get_layer_number_of_neuron is not None:
+                ret = self.mlp_network_get_layer_number_of_neuron(network['model'], i)
         return ret
 
     def mlGetNetworkLayerOutputSignal(self, network, i):
         res = None
 
-        if 'model' in network.keys():
+        with MLPModelManager(network, 'model') as model:
             numberOfNeuron = self.mlGetLayerNumberOfNeuron(network, i)
 
             if numberOfNeuron > 0:
@@ -237,7 +290,7 @@ class MLPlugin(MLPluginBase, MLPLoader):
 
     def mlGetNetworkInputSignal(self, network):
         res = None
-        if 'model' in network.keys():
+        with MLPModelManager(network, 'model') as model:
             numberOfInput = self.mlGetNetworkNumberOfInput(network)
 
             if numberOfInput > 0:
--- MLP/plugin/mlploader.py
+++ MLP/plugin/mlploader.py
@@ -15,51 +15,28 @@ def enum(*args):
     return type('Enum', (), values)
 
 class MLPLoader(MLLoader):
-    init                            = None
-    metadata                        = None
-    newTrainer                      = None
-    deleteTrainer                   = None
-    configureTrainer                = None
-    isTrainerRunning                = None
-    getTrainerProgress              = None
-    runTrainer                      = None
-    getTrainerError                 = None
-    saveTrainerProgress             = None
-    restoreTrainerProgress          = None
-    getTrainerManagedNetwork        = None
-    newNetwork                      = None
-    deleteNetwork                   = None
-    serializeNetwork                = None
-    deserializeNetwork              = None
-    predict                         = None
-    getNetworkOutput                = None
-    getNetworkOutputLength          = None
-    getNetworkLayerNumberOfNeuron   = None
-    getNetworkNumberOfLayer         = None
-    getNetworkNumberOfInput         = None
-
     def __init__(self):
         MLLoader.__init__(self, 'libMLP.so')
 
-        self.init                            = MLFunction(self, 'mlp_plugin_init',                         None,                       [])
-        self.metadata                        = MLFunction(self, 'mlp_plugin_metadata',                     ctypes.POINTER(MLPMetaData),[])
-        self.newTrainer                      = MLFunction(self, 'mlp_trainer_new',                         ctypes.POINTER(MLPTrainer), [ctypes.c_char_p, ctypes.c_char_p])
-        self.deleteTrainer                   = MLFunction(self, 'mlp_trainer_delete',                      None,                       [ctypes.POINTER(MLPTrainer)])
-        self.configureTrainer                = MLFunction(self, 'mlp_trainer_configure',                   None,                       [ctypes.POINTER(MLPTrainer), ctypes.c_char_p])
-        self.isTrainerRunning                = MLFunction(self, 'mlp_trainer_is_running',                  ctypes.c_ubyte,             [ctypes.POINTER(MLPTrainer)])
-        self.getTrainerProgress              = MLFunction(self, 'mlp_trainer_get_progress',                ctypes.c_double,            [ctypes.POINTER(MLPTrainer)])
-        self.runTrainer                      = MLFunction(self, 'mlp_trainer_run',                         None,                       [ctypes.POINTER(MLPTrainer)])
-        self.getTrainerError                 = MLFunction(self, 'mlp_trainer_error',                       ctypes.c_double,            [ctypes.POINTER(MLPTrainer)])
-        self.saveTrainerProgress             = MLFunction(self, 'mlp_trainer_save_progression',            None,                       [ctypes.POINTER(MLPTrainer), ctypes.c_char_p])
-        self.restoreTrainerProgress          = MLFunction(self, 'mlp_trainer_restore_progression',         None,                       [ctypes.POINTER(MLPTrainer), ctypes.c_char_p, ctypes.c_double, ctypes.c_double])
-        self.getTrainerManagedNetwork        = MLFunction(self, 'mlp_trainer_get_network',                 ctypes.POINTER(MLPNetwork), [ctypes.POINTER(MLPTrainer)])
-        self.newNetwork                      = MLFunction(self, 'mlp_network_new',                         ctypes.POINTER(MLPNetwork), [ctypes.c_char_p])
-        self.deleteNetwork                   = MLFunction(self, 'mlp_network_delete',                      None,                       [ctypes.POINTER(MLPNetwork)])
-        self.serializeNetwork                = MLFunction(self, 'mlp_network_serialize',                   None,                       [ctypes.POINTER(MLPNetwork), ctypes.c_char_p])
-        self.deserializeNetwork              = MLFunction(self, 'mlp_network_deserialize',                 None,                       [ctypes.POINTER(MLPNetwork), ctypes.c_char_p])
-        self.predict                         = MLFunction(self, 'mlp_network_predict',                     None,                       [ctypes.POINTER(MLPNetwork), ctypes.c_uint, ctypes.c_void_p])
-        self.getNetworkOutput                = MLFunction(self, 'mlp_network_get_output',                  ctypes.c_void_p,            [ctypes.POINTER(MLPNetwork)])
-        self.getNetworkOutputLength          = MLFunction(self, 'mlp_network_get_output_length',           ctypes.c_uint,              [ctypes.POINTER(MLPNetwork)])
-        self.getNetworkLayerNumberOfNeuron   = MLFunction(self, 'mlp_network_get_layer_number_of_neuron',  ctypes.c_uint,              [ctypes.POINTER(MLPNetwork), ctypes.c_uint])
-        self.getNetworkNumberOfLayer         = MLFunction(self, 'mlp_network_get_number_of_layer',         ctypes.c_uint,              [ctypes.POINTER(MLPNetwork)])
-        self.getNetworkNumberOfInput         = MLFunction(self, 'mlp_network_get_number_of_input',         ctypes.c_uint,              [ctypes.POINTER(MLPNetwork)])
+        self.mlp_plugin_init                       = MLFunction(self, 'mlp_plugin_init',                         None,                       [])
+        self.mlp_plugin_metadata                   = MLFunction(self, 'mlp_plugin_metadata',                     ctypes.POINTER(MLPMetaData),[])
+        self.mlp_trainer_new                       = MLFunction(self, 'mlp_trainer_new',                         ctypes.POINTER(MLPTrainer), [ctypes.c_char_p, ctypes.c_char_p])
+        self.mlp_trainer_delete                    = MLFunction(self, 'mlp_trainer_delete',                      None,                       [ctypes.POINTER(MLPTrainer)])
+        self.mlp_trainer_configure                 = MLFunction(self, 'mlp_trainer_configure',                   None,                       [ctypes.POINTER(MLPTrainer), ctypes.c_char_p])
+        self.mlp_trainer_is_running                = MLFunction(self, 'mlp_trainer_is_running',                  ctypes.c_ubyte,             [ctypes.POINTER(MLPTrainer)])
+        self.mlp_trainer_get_progress              = MLFunction(self, 'mlp_trainer_get_progress',                ctypes.c_double,            [ctypes.POINTER(MLPTrainer)])
+        self.mlp_trainer_run                       = MLFunction(self, 'mlp_trainer_run',                         None,                       [ctypes.POINTER(MLPTrainer)])
+        self.mlp_trainer_error                     = MLFunction(self, 'mlp_trainer_error',                       ctypes.c_double,            [ctypes.POINTER(MLPTrainer)])
+        self.mlp_trainer_save_progression          = MLFunction(self, 'mlp_trainer_save_progression',            None,                       [ctypes.POINTER(MLPTrainer), ctypes.c_char_p])
+        self.mlp_trainer_restore_progression       = MLFunction(self, 'mlp_trainer_restore_progression',         None,                       [ctypes.POINTER(MLPTrainer), ctypes.c_char_p, ctypes.c_double, ctypes.c_double])
+        self.mlp_trainer_get_network               = MLFunction(self, 'mlp_trainer_get_network',                 ctypes.POINTER(MLPNetwork), [ctypes.POINTER(MLPTrainer)])
+        self.mlp_network_new                       = MLFunction(self, 'mlp_network_new',                         ctypes.POINTER(MLPNetwork), [ctypes.c_char_p])
+        self.mlp_network_delete                    = MLFunction(self, 'mlp_network_delete',                      None,                       [ctypes.POINTER(MLPNetwork)])
+        self.mlp_network_serialize                 = MLFunction(self, 'mlp_network_serialize',                   None,                       [ctypes.POINTER(MLPNetwork), ctypes.c_char_p])
+        self.mlp_network_deserialize               = MLFunction(self, 'mlp_network_deserialize',                 None,                       [ctypes.POINTER(MLPNetwork), ctypes.c_char_p])
+        self.mlp_network_predict                   = MLFunction(self, 'mlp_network_predict',                     None,                       [ctypes.POINTER(MLPNetwork), ctypes.c_uint, ctypes.c_void_p])
+        self.mlp_network_get_output                = MLFunction(self, 'mlp_network_get_output',                  ctypes.c_void_p,            [ctypes.POINTER(MLPNetwork)])
+        self.mlp_network_get_output_length         = MLFunction(self, 'mlp_network_get_output_length',           ctypes.c_uint,              [ctypes.POINTER(MLPNetwork)])
+        self.mlp_network_get_layer_number_of_neuron= MLFunction(self, 'mlp_network_get_layer_number_of_neuron',  ctypes.c_uint,              [ctypes.POINTER(MLPNetwork), ctypes.c_uint])
+        self.mlp_network_get_number_of_layer       = MLFunction(self, 'mlp_network_get_number_of_layer',         ctypes.c_uint,              [ctypes.POINTER(MLPNetwork)])
+        self.mlp_network_get_number_of_input       = MLFunction(self, 'mlp_network_get_number_of_input',         ctypes.c_uint,              [ctypes.POINTER(MLPNetwork)])
